19ea0d283e936368a02434a858012cfd90564fdc
Akhmadillo Mamirov <akhmadillomamirov@Akhmadillos-MacBook-Air.local>
2023-04-30 21:53:05 -0400

    adding todo toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import UseRefExample2 from './components/UseRefExample2';
+import UseRefExample3 from './components/UseRefExample3';
 
 function App() {
   return (
     <div className="container mt-5">
-        <UseRefExample2 />
+        <UseRefExample3 />
     </div>
   );
 }
